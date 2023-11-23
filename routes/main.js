9d6fc1b4ba8648482faccc0e920170d767010eaa
anis10023 <m.anis.rahman@outlook.com>
2023-11-23 13:11:10 +0000

    Completed with extension

--- routes/main.js
+++ routes/main.js
@@ -1,34 +1,50 @@
+// Exporting a function that sets up routes for the application
 module.exports = function (app, shopData) {
-  app.get("/list", function (req, res) {
-    let sqlquery = "SELECT * FROM books"; // query database to get all the books
-    // execute sql query
-    db.query(sqlquery, (err, result) => {
-      if (err) {
-        res.redirect("./");
-      }
-      res.send(result);
-    });
-  });
-
-  // Handle our routes
+  // Home route
   app.get("/", function (req, res) {
     res.render("index.ejs", shopData);
   });
+
+  // About route
   app.get("/about", function (req, res) {
     res.render("about.ejs", shopData);
   });
+
+  // Search route
   app.get("/search", function (req, res) {
-    res.render("search.ejs", shopData);
+    // Extract the search query from the URL
+    const searchQuery = req.query.query;
+
+    if (!searchQuery) {
+      // If no search query is provided, render the search form
+      return res.render("search.ejs", { shopName: "Bertie's Book" });
+    }
+
+    // Perform a database query to find books matching the search query
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    let searchPattern = `%${searchQuery}%`;
+
+    db.query(sqlquery, [searchPattern], (err, result) => {
+      if (err) {
+        console.error("Error executing SQL query:", err);
+        return res.status(500).send("Error fetching data from the database.");
+      }
+
+      // Render the search results page with the matching books
+      res.render("search-result.ejs", {
+        shopName: "Bertie's Book",
+        availableBooks: result,
+      });
+    });
   });
-  app.get("/search-result", function (req, res) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword);
-  });
+
+  // Register route
   app.get("/register", function (req, res) {
     res.render("register.ejs", shopData);
   });
+
+  // Registered route
   app.post("/registered", function (req, res) {
-    // saving data in database
     res.send(
       " Hello " +
         req.body.first +
@@ -37,5 +53,63 @@ module.exports = function (app, shopData) {
         " you are now registered!  We will send an email to you at " +
         req.body.email
     );
+  });
+
+  // List route
+  app.get("/list", function (req, res) {
+    // Query database to get all the books
+    let sqlquery = "SELECT * FROM books";
+
+    // Execute SQL query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("./");
+      }
+      let newData = Object.assign({}, shopData, { availableBooks: result });
+      console.log(newData);
+      res.render("list.ejs", newData);
+    });
+  });
+
+  // Addbook route
+  app.get("/addbook", function (req, res) {
+    res.render("addbook.ejs", shopData);
+  });
+
+  // Bookadded route
+  app.post("/bookadded", function (req, res) {
+    // Saving data in the database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+
+    // Execute SQL query
+    let newrecord = [req.body.name, req.body.price];
+    db.query(sqlquery, newrecord, (err, result) => {
+      if (err) {
+        return console.error(err.message);
+      } else {
+        res.send(
+          " This book is added to database, book name: " +
+            req.body.name +
+            " price " +
+            req.body.price
+        );
+      }
+    });
+  });
+
+  // Bargainbooks route
+  app.get("/bargainbooks", function (req, res) {
+    // Query database to get all the books with price less than 20
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+
+    // Execute SQL query
+    db.query(sqlquery, (err, result) => {
+      if (err) {
+        res.redirect("./");
+      }
+      let newData = Object.assign({}, shopData, { availableBooks: result });
+      console.log(newData);
+      res.render("bargainbooks.ejs", newData);
+    });
   });
 };
